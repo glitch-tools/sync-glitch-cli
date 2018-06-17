205d1abb59afc4bc4895b0ca9bf3f327acefcfbb
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2018-06-17 17:19:48 +0900

    refactor(files): with pretteir

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,21 +1,21 @@
-const GlitchApi = require('../lib/glitch.js')
-const nock = require('nock')
+const GlitchApi = require("../lib/glitch.js");
+const nock = require("nock");
 
-const token = '1234'
-const projectId = '1234'
-const repo = 'me/repo'
+const token = "1234";
+const projectId = "1234";
+const repo = "me/repo";
 
-describe('#importFromGithub using async/await', () => {
+describe("#importFromGithub using async/await", () => {
   beforeEach(() => {
-    nock('https://api.glitch.com')
-      .post('/project/githubImport')
-      .query({token: token, projectId: projectId, repo: repo })
-      .reply(200, {status: 200})
-  })
+    nock("https://api.glitch.com")
+      .post("/project/githubImport")
+      .query({ authorization: token, projectId: projectId, repo: repo })
+      .reply(200, { status: 200 });
+  });
 
-  test('200', async () => {
-    const glitchApi = new GlitchApi(token)
-    const response = await glitchApi.importFromGithub(projectId, repo)
-    expect(response.status).toBe(200)
-  })
-}) // #importFromGitHub
+  test("200", async () => {
+    const glitchApi = new GlitchApi(token);
+    const response = await glitchApi.importFromGithub(projectId, repo);
+    expect(response.status).toBe(200);
+  });
+}); // #importFromGitHub
