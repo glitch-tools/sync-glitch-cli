aa1de48b0fbc9c3acbf5a27e505d458e2e91488d
Hoon Kim <hoonsubin@icloud.com>
2020-10-26 01:30:31 -0700

    feat(param): added optional repo subdir param

--- cli.js
+++ cli.js
@@ -1,37 +1,43 @@
 #!/usr/bin/env node
-const GlitchAPI = require('./lib/glitch.js')
-const envalid = require('envalid')
-const chalk = require('chalk')
-const debug = require('debug')('sync-glitch')
+const GlitchAPI = require("./lib/glitch.js");
+const envalid = require("envalid");
+const chalk = require("chalk");
+const debug = require("debug")("sync-glitch");
 
 // Quote from gr2m/glitch-deploy
 // https://github.com/gr2m/glitch-deploy/blob/master/bin/glitch-deploy.js#L5-L8
 if (process.env.CI) {
-  debug('Running on CI and checking for environment variables.')
+  debug("Running on CI and checking for environment variables.");
   envalid.cleanEnv(process.env, {
     GH_REPO: envalid.str(),
     GLITCH_TOKEN: envalid.str(),
-    GLITCH_PROJECT_ID: envalid.str()
-  })
+    GLITCH_PROJECT_ID: envalid.str(),
+    //note: we skip the GH_SUBDIR variable because it will default to '/' (root)
+  });
 }
 
-;(async () => {
-  const glitchClient = new GlitchAPI(process.env.GLITCH_TOKEN)
-  const response = await glitchClient.importFromGithub(process.env.GLITCH_PROJECT_ID, process.env.GH_REPO)
+(async () => {
+  const glitchClient = new GlitchAPI(process.env.GLITCH_TOKEN);
+  const response = await glitchClient.importFromGithub(
+    process.env.GLITCH_PROJECT_ID,
+    process.env.GH_REPO,
+    // the subdirectory is an optional parameter
+    process.env.GH_SUBDIR || undefined
+  );
 
   if (response.status !== 200) {
     const output = [
-      chalk.red('error '),
-      'Fail to sync changes in your GitHub repository to glitch.com',
-      chalk.yellow('\nExiting with error code 1')
-    ].join('')
-    console.error(output)
-    process.exit(1)
+      chalk.red("error "),
+      "Fail to sync changes in your GitHub repository to glitch.com",
+      chalk.yellow("\nExiting with error code 1"),
+    ].join("");
+    console.error(output);
+    process.exit(1);
   } else {
     const output = [
-      chalk.green('success '),
-      'To sync changes in your GitHub repository to glitch.com'
-    ].join('')
-    console.log(output)
+      chalk.green("success "),
+      "Synced changes in your GitHub repository to glitch.com",
+    ].join("");
+    console.log(output);
   }
-})()
+})();
--- test/basic.test.js
+++ test/basic.test.js
@@ -4,18 +4,47 @@ const nock = require("nock");
 const token = "1234";
 const projectId = "1234";
 const repo = "me/repo";
+const path = "folder";
 
 describe("#importFromGithub using async/await", () => {
   beforeEach(() => {
-    nock("https://api.glitch.com")
-      .post("/project/githubImport")
-      .query({ authorization: token, projectId: projectId, repo: repo })
-      .reply(200, { status: 200 });
+    nock.disableNetConnect();
   });
 
   test("200", async () => {
+    nock("https://api.glitch.com")
+      .post("/project/githubImport")
+      .query({
+        authorization: token,
+        projectId: projectId,
+        repo: repo,
+        path: path
+      })
+      .reply(200, { status: 200 });
+
+    const glitchApi = new GlitchApi(token);
+    const response = await glitchApi.importFromGithub(projectId, repo, path);
+    expect(response.status).toBe(200);
+  });
+
+  test("no repo subdirectory", async () => {
+    nock("https://api.glitch.com")
+      .post("/project/githubImport")
+      .query({
+        authorization: token,
+        projectId: projectId,
+        repo: repo,
+        path: "/" // default path
+      })
+      .reply(200, { status: 200 });
+
     const glitchApi = new GlitchApi(token);
     const response = await glitchApi.importFromGithub(projectId, repo);
     expect(response.status).toBe(200);
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+    nock.enableNetConnect();
   });
 }); // #importFromGitHub
